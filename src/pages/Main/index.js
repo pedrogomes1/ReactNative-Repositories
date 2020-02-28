ff16fb0d1a866c712571d76a6da903980eb91d34
Pedro <pedro-gomes17@hotmail.com>
2020-02-28 16:45:37 -0300

    Adicionado exclusão do repositório e validação para não adicionar repositório já existente

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,3 +1,4 @@
+/* eslint-disable react/no-did-mount-set-state */
 import React, {Component} from 'react';
 import {
   Container,
@@ -12,11 +13,12 @@ import {
   ProfileButton,
   ProfileButtonText,
   Loading,
+  DeleteButton,
 } from './styles';
 import PropTypes from 'prop-types';
 import api from '../../services/api';
 import AsyncStorage from '@react-native-community/async-storage';
-import {Keyboard, ActivityIndicator} from 'react-native';
+import {Keyboard, ActivityIndicator, Alert} from 'react-native';
 
 import Icon from 'react-native-vector-icons/MaterialIcons';
 
@@ -38,7 +40,6 @@ export default class Main extends Component {
 
   async componentDidMount() {
     const users = await AsyncStorage.getItem('users');
-
     if (users) {
       this.setState({users: JSON.parse(users)});
     }
@@ -51,26 +52,49 @@ export default class Main extends Component {
       AsyncStorage.setItem('users', JSON.stringify(users));
     }
   }
-  handleSubmit = async () => {
+  handleSubmit = async nameUser => {
+    console.tron.log(this.state.users);
     const {users, newUser} = this.state;
 
     const response = await api.get(`/users/${newUser}`);
 
-    const data = {
-      name: response.data.name,
-      login: response.data.login,
-      bio: response.data.bio,
-      avatar: response.data.avatar_url,
-    };
-    this.setState({users: [...users, data], newUser: ''});
+    const findUsers = users.findIndex(user => user.login === nameUser);
+    console.tron.log(findUsers);
+    if (!(findUsers >= 0)) {
+      const data = {
+        name: response.data.name,
+        login: response.data.login,
+        bio: response.data.bio,
+        avatar: response.data.avatar_url,
+      };
+      this.setState({users: [...users, data], newUser: ''});
 
-    Keyboard.dismiss(); //Quando digitiar o usuário o teclado some
+      Keyboard.dismiss(); //Quando digitiar o usuário o teclado some
+    } else {
+      return Alert.alert(`O usuário ${nameUser} já foi adicionado!`);
+    }
   };
 
   handleNavigate = user => {
     const {navigation} = this.props;
 
     navigation.navigate('User', {user});
+  };
+
+  deleteRepository = name => {
+    const {users} = this.state;
+
+    //Preciso criar uma cópia do array do estado, não posso alterar diretamente o original (imutabilidade)
+    var arrayUsers = [...users];
+    const verifyUser = arrayUsers.findIndex(user => user.name === name);
+
+    //Retorna 0 se for true, ou seja, se for o usuario selecionado
+    if (verifyUser !== -1) {
+      arrayUsers.splice(verifyUser, 1);
+      this.setState({users: arrayUsers});
+    }
+
+    console.tron.log('users', users);
   };
 
   render() {
@@ -89,7 +113,9 @@ export default class Main extends Component {
             onSubmitEditing={this.handleSubmit} //Quando clicar no botão send do teclado ele chama a função tb
           />
 
-          <SubmitButton onPress={this.handleSubmit} loading={loading}>
+          <SubmitButton
+            onPress={() => this.handleSubmit(newUser)}
+            loading={loading}>
             {loading ? (
               <ActivityIndicator color="fff" />
             ) : (
@@ -103,19 +129,25 @@ export default class Main extends Component {
         ) : (
           <List
             data={users} //Onde vão estar os dados dessa minha lista, precisa ser um array
-            keyExtractor={user => user.login}
+            keyExtractor={user => String(user.name)}
             renderItem={(
               {item}, //Esse item desestruturado é do user
             ) => (
-              <User>
-                <Avatar source={{uri: item.avatar}} />
-                <Name>{item.name}</Name>
-                <Bio>{item.bio}</Bio>
+              <>
+                <User>
+                  <DeleteButton
+                    onPress={() => this.deleteRepository(item.name)}>
+                    <Icon name="delete" size={30} color="#FF0000" />
+                  </DeleteButton>
+                  <Avatar source={{uri: item.avatar}} />
+                  <Name>{item.name}</Name>
+                  <Bio>{item.bio}</Bio>
 
-                <ProfileButton onPress={() => this.handleNavigate(item)}>
-                  <ProfileButtonText>Ver perfil</ProfileButtonText>
-                </ProfileButton>
-              </User>
+                  <ProfileButton onPress={() => this.handleNavigate(item)}>
+                    <ProfileButtonText>Ver perfil</ProfileButtonText>
+                  </ProfileButton>
+                </User>
+              </>
             )}
           />
         )}
